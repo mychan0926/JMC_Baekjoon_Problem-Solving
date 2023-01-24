3ce2fff0d0481b132dbdf82a37ea044b29f4a4da
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:42:27 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a=int(input())
+if 100>=a and a>=90:
+    print("A")
+elif 90>a and a>=80:
+    print("B")
+elif 80>a and a>=70:
+    print("C")
+elif 70>a and a>=60:
+    print("D")
+else:
+    print("F")
