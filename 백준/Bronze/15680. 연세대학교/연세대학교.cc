1215ad5efa88298fef8738c8517145346355f475
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:49:57 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+    int a,b;
+    cin>>a;
+    
+    if(a==0)
+    {
+        cout<< "YONSEI";
+    }
+    else
+    {
+        cout<< "Leading the Way to the Future";
+    }
+}
