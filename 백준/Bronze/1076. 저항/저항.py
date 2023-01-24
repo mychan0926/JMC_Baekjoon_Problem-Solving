3e94c551f7f89029776df77ba2b54f1d15f35897
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:17:10 +0900

    [Bronze II] Title: 저항, Time: 104 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+A=input()
+B=input()
+C=input()
+E={'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'violet':7,'grey':8,'white':9} 
+F={'black':1,'brown':10,'red':100,'orange':1000,'yellow':10000,'green':100000,'blue':1000000,'violet':10000000,'grey':100000000,'white':1000000000}
+print(F[C]*(E[A]*10+E[B]))
