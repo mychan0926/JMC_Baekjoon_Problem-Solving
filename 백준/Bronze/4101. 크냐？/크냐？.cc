948aaaf4f0fd539a99b39c69007609f1715ce0fb
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:38:56 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+    int a,b;
+    cin >>a>>b;
+    if(a>b)
+        
+    {
+        cout <<"Yes"<<"\n";
+    }
+    else if (a==0&&b==0)
+        {
+        break;
+    }
+    else if(b>=a)
+    {
+        cout <<"No"<<"\n";
+    }
+    }
+}
