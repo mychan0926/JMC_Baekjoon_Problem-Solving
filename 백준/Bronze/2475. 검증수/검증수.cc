81af64046cb496af90b947057cb646fd5d0b9c6b
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:07:22 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a,b=0;
+   
+    for (int i=0;i<5;i++)
+    {
+        cin >> a;
+        b += a * a;
+    }
+    cout << b % 10;
+
+
+}
