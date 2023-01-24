a5ee10a591f37e0cc8ab736bd5ba3b6c0ab71854
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:38:49 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,19 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+
+using namespace std;
+int main()
+{
+
+
+	set <double> s;
+	double a;
+	int x, y;
+	double pi = M_PI;
+	cout.precision(30);
+	cin >> a;
+	cout << pi * a*a << "\n" << ((2 * a) * (2 * a) )/ 2;
+}
