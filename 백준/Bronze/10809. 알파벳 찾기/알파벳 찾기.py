6837f1e9a4daa96eecc307c03aadf8b153950e65
mychan0926 <98321404+mychan0926@users.noreply.github.com>
2023-01-25 03:44:19 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 112 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,6 @@
+a=input()
+b=[]
+for i in 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z':
+    b.append(a.find(i))
+for i in range(len(b)):
+    print(b[i])
