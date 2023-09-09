76ce1998b87aa949a8a6193600f014bb815d852d
no_name <mychan0926@naver.com>
2023-09-10 00:36:11 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <set>
+#include <map>
+using namespace std;
+
+int main()
+{
+cout<<"  ___  ___  ___\n" ;
+cout<<"  | |__| |__| |\n" ;
+cout<<"  |           |\n" ;
+cout<<"   \\_________/\n" ;
+cout<<"    \\_______/\n" ;
+cout<<"     |     |\n" ;
+cout<<"     |     |\n" ;
+cout<<"     |     |\n" ;
+cout<<"     |     |\n" ;
+cout<<"     |_____|\n" ;
+cout<<"  __/       \\__\n" ;
+cout<<" /             \\\n" ;
+cout<<"/_______________\\\n" ;
+}
