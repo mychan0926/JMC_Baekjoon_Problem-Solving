f659dbafbdc5933bdd566d72ed25d68b69065f75
no_name <mychan0926@naver.com>
2023-09-17 19:58:03 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <queue>
+
+
+using namespace std;
+
+
+int main()
+    
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<m-n<<" "<<m;
+    
+}
