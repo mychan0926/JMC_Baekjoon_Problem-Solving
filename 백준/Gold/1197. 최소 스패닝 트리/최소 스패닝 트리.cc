1d4000a6c7e297d9199fa72176d49d2ee8bc9a80
Jung Myeong Chan <mychan0926@naver.com>
2023-10-18 12:03:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 764 ms, Memory: 8648 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+vector <long long> v;
+vector <vector <long long>> s;
+long long counting = 0;
+long long t(long long a, long long b)
+{
+	int t1 = a;
+	int t2 = b;
+	while (1){
+		if (t1 == v[t1]){
+			break;
+		}
+		else{
+			t1 = v[t1];
+		}
+	}
+	while (1){
+		if (t2 == v[t2]){
+			break;
+		}
+		else{
+			t2 = v[t2];
+		}
+	}
+	if (t2 == t1)
+	{
+		return 1;
+	}
+	else
+	{
+		v[t2] = t1;
+		return 0;
+	}
+
+
+}
+
+int main() {
+
+	long long N,M;
+	cin >> N>>M;
+	v.resize(N,0);
+	for (int i = 0; i < v.size(); i++)
+	{
+		v[i] = i;
+	}
+	for (int i = 0; i < M; i++)
+	{
+		int a, b,c;
+		cin >> a >> b>>c;
+		s.push_back({c,a-1,b-1});
+	}
+	sort(s.begin(),s.end());
+	for (int i = 0; i < M;i++)
+	{
+		if (t(s[i][1], s[i][2]) == 0)
+		{
+			counting += s[i][0];
+		}
+	}
+	cout << counting;
+
+
+}
