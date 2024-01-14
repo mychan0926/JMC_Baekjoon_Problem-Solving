ed8f09e3d4d84b456ee103342c1f1a35e6a11577
Jung Myeong Chan <mychan0926@naver.com>
2024-01-14 21:43:24 +0900

    [Bronze V] Title: Helicopter, Time: 60 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,11 @@
+Public Module Program
+	Public Sub Main(args() As string)
+		Console.WriteLine("      ===================")
+    Console.WriteLine("          ____||___")
+    Console.WriteLine("\ /      /       []\")
+    Console.WriteLine(" X=======           \__")
+    Console.WriteLine("/ \      \____________|")
+    Console.WriteLine("            ||  ||")
+    Console.WriteLine("         \-----------/")
+	End Sub
+End Module
