e5eff0c34edae2f54fd7faecaa959bb2486018ff
Jung Myeong Chan <mychan0926@naver.com>
2025-10-04 23:07:12 +0900

    [Gold IV] Title: N-Queen, Time: 1632 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,52 +1,48 @@
 #include <iostream>
-#include <set>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
-vector <vector <int>> chess;
-vector <int> D1(15,0);
-vector <int> DRUP(27,0);
-vector <int> DLUP(27,0);
+
+vector <int> width;
+vector <int> left_rising_diagonal;
+vector <int> right_rising_diagonal;
 int N;
 int cnt = 0;
 
-void queen(int t)
+void queen(int count)
 {
-	if (t == N)
+	if (count == N)
 	{
 		cnt++;
 		return;
 	}
 	else
 	{
-		for (int i = 0; i < N; i++)
+		for (int i=0;i<N;i++)
 		{
-			if (D1[i] == 0 && DRUP[i + t] == 0 && DLUP[i - t + N - 1] ==0)
+			if (width[i] == 0 && right_rising_diagonal[count+i]==0&& left_rising_diagonal[count-i+N] == 0)
 			{
-				D1[i] += 1;
-				DRUP[i+t] += 1;
-				DLUP[i - t + N - 1] += 1;
-				queen(t + 1);
-				D1[i] -= 1;
-				DRUP[i + t] -= 1;
-				DLUP[i - t + N - 1] -= 1;
-
-
+				width[i] = 1;
+				right_rising_diagonal[count + i] = 1;
+				left_rising_diagonal[count - i + N] = 1;
+				queen(count + 1);
+				width[i] = 0;
+				right_rising_diagonal[count + i] = 0;
+				left_rising_diagonal[count - i + N] = 0;
 			}
 		}
+
+
 	}
-	return;
 }
+
 int main()
 {
-	int M;
 	cin >> N;
-	
-	for (int i = 0; i < N; i++)
-	{
-		vector <int> tem(N, 0);
-		chess.push_back(tem);
-	}
+	width.resize(N,0);
+	left_rising_diagonal.resize(N+N, 0);
+	right_rising_diagonal.resize(N+N, 0);
 	queen(0);
 	cout << cnt;
 }
+
